e35414aa544850cb1e0f857eedb7d44a051e74d7
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-02-03 22:02:56 +0600

    4th

--- nameconstractor.dart
+++ nameconstractor.dart
@@ -3,24 +3,23 @@ class Car {
   String? color;
   int? prize;
 
-  Car({this.name,this.color,this.prize});
-  Car.details({this.name,this.color});
+  Car({this.name, this.color, this.prize});
+  Car.details({this.name, this.color});
 
-  void Display(){
-    if(prize == null){
-      prize=0;
+  void Display() {
+    if (prize == null) {
+      prize = 0;
     }
     print('Car Name : $name');
     print('Car Name : $color');
     print('Car Name : $prize');
-    
   }
 }
 
-void main(){
-  Car car1 = Car(color: 'White',name: 'Audi',prize: 100);
+void main() {
+  Car car1 = Car(color: 'White', name: 'Audi', prize: 100);
   car1.Display();
 
-  Car car2 = Car(color: 'Black',name: 'Mazda');
+  Car car2 = Car.details(color: 'Black', name: 'Mazda');
   car2.Display();
 }
---
+++ static.dart
@@ -0,0 +1,22 @@
+class Employee {
+  // Static variable
+  static int count = 0;
+  // Constructor
+  Employee() {
+    count++;
+  }
+  // Method to display the value of count
+  void totalEmployee() {
+    print("Total Employee: $count");
+  }
+}
+
+void main() {
+  Employee e1 = new Employee();
+  e1.totalEmployee();
+  Employee e2 = new Employee();
+  e2.totalEmployee();
+  Employee e3 = new Employee();
+  e3.totalEmployee();
+  e1.totalEmployee();
+}
